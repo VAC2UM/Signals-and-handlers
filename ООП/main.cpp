892540bbba9b57a7678916eed549f4621d0d5365
VAC2UM <119112926+VAC2UM@users.noreply.github.com>
2023-05-19 17:14:44 +0300

    Update main.cpp

--- ООП/main.cpp
+++ ООП/main.cpp
@@ -2,7 +2,7 @@
 #include "cl_application.h"
 using namespace std;
 int main() {
-    cl_application  ob_cl_application(nullptr);   // создание корневого объекта
-    ob_cl_application.build_tree_objects();       // конструирование системы, построение дерева объектов
-    return ob_cl_application.exec_app();         // запуск системы
+    cl_application  ob_cl_application(nullptr);
+    ob_cl_application.build_tree_objects();
+    return ob_cl_application.exec_app();
 }
